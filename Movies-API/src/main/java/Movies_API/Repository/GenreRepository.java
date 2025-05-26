4885415abe7b790c63ec6bc4454518294b531617
Andero <andero.sc@gmail.com>
2025-05-26 15:13:37 +0300

    test

--- Movies-API/src/main/java/Movies_API/Repository/GenreRepository.java
+++
@@ -1,7 +0,0 @@
-package Movies_API.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import Movies_API.model.Genre;
-
-public interface GenreRepository extends JpaRepository<Genre, Long> {
-}
